bfd8f20b5fdff55fc15e5be97336f64c98652176
djadjagit <djadja@ngs.ru>
2017-06-14 02:52:22 +0300

    Добавьте файлы проекта.

---
+++ clicker/Form1.Designer.cs
@@ -0,0 +1,104 @@
+﻿namespace clicker
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.notifyIcon1 = new System.Windows.Forms.NotifyIcon(this.components);
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.toolStripTextBox1 = new System.Windows.Forms.ToolStripTextBox();
+            this.toolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripSeparator1 = new System.Windows.Forms.ToolStripSeparator();
+            this.contextMenuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // notifyIcon1
+            // 
+            this.notifyIcon1.ContextMenuStrip = this.contextMenuStrip1;
+            this.notifyIcon1.Icon = ((System.Drawing.Icon)(resources.GetObject("notifyIcon1.Icon")));
+            this.notifyIcon1.Visible = true;
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripTextBox1,
+            this.toolStripSeparator1,
+            this.toolStripMenuItem1});
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(261, 57);
+            // 
+            // toolStripTextBox1
+            // 
+            this.toolStripTextBox1.Enabled = false;
+            this.toolStripTextBox1.Name = "toolStripTextBox1";
+            this.toolStripTextBox1.Size = new System.Drawing.Size(200, 23);
+            // 
+            // toolStripMenuItem1
+            // 
+            this.toolStripMenuItem1.Name = "toolStripMenuItem1";
+            this.toolStripMenuItem1.Size = new System.Drawing.Size(260, 22);
+            this.toolStripMenuItem1.Text = "Выход";
+            this.toolStripMenuItem1.Click += new System.EventHandler(this.toolStripMenuItem1_Click);
+            // 
+            // toolStripSeparator1
+            // 
+            this.toolStripSeparator1.Name = "toolStripSeparator1";
+            this.toolStripSeparator1.Size = new System.Drawing.Size(257, 6);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(364, 308);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Activated += new System.EventHandler(this.Form1_Activated);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.contextMenuStrip1.ResumeLayout(false);
+            this.contextMenuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.NotifyIcon notifyIcon1;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.ToolStripTextBox toolStripTextBox1;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem1;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator1;
+    }
+}
+
---
+++ clicker/Form1.cs
@@ -0,0 +1,165 @@
+﻿using System;
+using System.Windows.Forms;
+using System.Runtime.InteropServices;
+using System.Diagnostics;
+
+
+namespace clicker
+{
+    public partial class Form1 : Form
+    {
+        [DllImport("user32.dll")]
+        public static extern void SetCursorPos(int x, int y);
+        [DllImport("user32.dll", CharSet = CharSet.Auto, CallingConvention = CallingConvention.StdCall)]
+        public static extern void mouse_event(int dwFlags, int dx, int dy, int cButtons, int dwExtraInfo);
+        private const int MOUSEEVENTF_LEFTDOWN = 0x02;
+        private const int MOUSEEVENTF_LEFTUP = 0x04;
+        private const int MOUSEEVENTF_RIGHTDOWN = 0x08;
+        private const int MOUSEEVENTF_RIGHTUP = 0x10;
+        [DllImport("user32.dll")]
+        public static extern void keybd_event(Keys bVk, byte bScan, UInt32 dwFlags, IntPtr dwExtraInfo);
+        public const UInt32 KEYEVENTF_EXTENDEDKEY = 0x01;
+        public const UInt32 KEYEVENTF_KEYUP = 0x02;
+        int flag = 0;
+        int i = 0;
+
+        public Form1()
+        {
+            InitializeComponent();
+            KeyboardHook.Start();
+            KeyboardHook.KeyboardAction += new KeyEventHandler(EventCtrl);
+        }
+
+        private void EventCtrl(object sender, KeyEventArgs e)
+        {
+//            textBox1.Text = e.KeyValue.ToString()+"; Alt:"+e.Alt.ToString() + "; Ctrl:" + e.Control.ToString() + "; Shift:" + e.Shift.ToString();
+        if (e.KeyValue==163) //правый Ctrl включает кликалку
+            {
+                flag = 1;
+                i = 0;
+                timer1.Enabled = true;
+                notifyIcon1.Icon = Resource1.NormalIcon;
+                toolStripTextBox1.Text = i.ToString() + ";" + timer1.Interval.ToString() + ";" + flag.ToString() + "; X:" + Cursor.Position.X.ToString() +
+                    "; Y:" + Cursor.Position.Y.ToString();
+                notifyIcon1.Text = i.ToString() + ";" + timer1.Interval.ToString() + ";" + flag.ToString() + "; X:" + Cursor.Position.X.ToString() +
+                    "; Y:" + Cursor.Position.Y.ToString() + "\r\nВкл - правый Ctrl; Выкл - левый Ctrl";
+            }
+            if (e.KeyValue == 162) //левый Ctrl выключает кликалку
+            {
+                flag = 0;
+                timer1.Enabled = false;
+                notifyIcon1.Icon = Resource1.DeletedIcon;
+                toolStripTextBox1.Text = i.ToString() + ";" + timer1.Interval.ToString() + ";" + flag.ToString() + "; X:" + Cursor.Position.X.ToString() +
+                    "; Y:" + Cursor.Position.Y.ToString();
+                notifyIcon1.Text = i.ToString() + ";" + timer1.Interval.ToString() + ";" + flag.ToString() + "; X:" + Cursor.Position.X.ToString() +
+                    "; Y:" + Cursor.Position.Y.ToString() + "\r\nВкл - правый Ctrl; Выкл - левый Ctrl";
+            }
+            if (e.KeyValue == 38) //Up увеличивает интервал клика
+            {
+                timer1.Interval = timer1.Interval + 100;
+                toolStripTextBox1.Text = i.ToString() + ";" + timer1.Interval.ToString() + ";" + flag.ToString() + "; X:" + Cursor.Position.X.ToString() +
+                    "; Y:" + Cursor.Position.Y.ToString();
+                notifyIcon1.Text = i.ToString() + ";" + timer1.Interval.ToString() + ";" + flag.ToString() + "; X:" + Cursor.Position.X.ToString() +
+                    "; Y:" + Cursor.Position.Y.ToString() + "\r\nВкл - правый Ctrl; Выкл - левый Ctrl";
+            }
+            if (e.KeyValue == 40) //Down уменьшает интервал клика
+            {
+                timer1.Interval = timer1.Interval - 100;
+                toolStripTextBox1.Text = i.ToString() + ";" + timer1.Interval.ToString() + ";" + flag.ToString() + "; X:" + Cursor.Position.X.ToString() +
+                    "; Y:" + Cursor.Position.Y.ToString();
+                notifyIcon1.Text = i.ToString() + ";" + timer1.Interval.ToString() + ";" + flag.ToString() + "; X:" + Cursor.Position.X.ToString() +
+                    "; Y:" + Cursor.Position.Y.ToString() + "\r\nВкл - правый Ctrl; Выкл - левый Ctrl";
+            }
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            timer1.Enabled = false;
+            i++;
+            mouse_event(MOUSEEVENTF_LEFTDOWN | MOUSEEVENTF_LEFTUP, Cursor.Position.X, Cursor.Position.Y, 0, 0);
+            this.Text =i.ToString()+";"+ timer1.Interval.ToString() + ";" + flag.ToString() + "; X:" + Cursor.Position.X.ToString() +
+                "; Y:" + Cursor.Position.Y.ToString();
+            notifyIcon1.Text = i.ToString() + ";" + timer1.Interval.ToString() + ";" + flag.ToString() + "; X:" + Cursor.Position.X.ToString() +
+                "; Y:" + Cursor.Position.Y.ToString() + "\r\nВкл - правый Ctrl; Выкл - левый Ctrl";
+            if (flag == 1) timer1.Enabled = true;
+        }
+
+        public static class KeyboardHook
+        {
+            private const int WH_KEYBOARD_LL = 13;
+            private const int WM_KEYDOWN = 0x0100;
+            private static LowLevelKeyboardProc _proc = HookCallback;
+            private static IntPtr _hookID = IntPtr.Zero;
+
+            public static event KeyEventHandler KeyboardAction = delegate { };
+
+            public static void Start()
+            {
+                _hookID = SetHook(_proc);
+            }
+            public static void stop()
+            {
+                UnhookWindowsHookEx(_hookID);
+            }
+
+            private static IntPtr SetHook(LowLevelKeyboardProc proc)
+            {
+                using (Process curProcess = Process.GetCurrentProcess())
+                using (ProcessModule curModule = curProcess.MainModule)
+                {
+                    return SetWindowsHookEx(WH_KEYBOARD_LL, proc, GetModuleHandle(curModule.ModuleName), 0);
+                }
+            }
+
+            private delegate IntPtr LowLevelKeyboardProc(int nCode, IntPtr wParam, IntPtr lParam);
+
+            private static IntPtr HookCallback(int nCode, IntPtr wParam, IntPtr lParam)
+            {
+                if ((nCode >= 0) && (wParam == (IntPtr)WM_KEYDOWN))
+                {
+                    int vkCode = Marshal.ReadInt32(lParam);
+//                    if ((vkCode > 36) && (vkCode < 41))
+                    if (vkCode > 10)
+                    {
+                        KeyEventArgs key = new KeyEventArgs((Keys)vkCode);
+                        KeyboardAction(null, key);
+                        return (IntPtr)1;
+                    }
+                }
+                return CallNextHookEx(_hookID, nCode, wParam, lParam);
+            }
+
+            [DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+            private static extern IntPtr SetWindowsHookEx(int idHook, LowLevelKeyboardProc lpfn, IntPtr hMod, uint dwThreadId);
+
+            [DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+            private static extern bool UnhookWindowsHookEx(IntPtr hhk);
+
+            [DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+            private static extern IntPtr CallNextHookEx(IntPtr hhk, int nCode, IntPtr wParam, IntPtr lParam);
+
+            [DllImport("kernel32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+            private static extern IntPtr GetModuleHandle(string lpModuleName);
+        }
+
+        private void toolStripMenuItem1_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            notifyIcon1.Icon = Resource1.DeletedIcon;
+            toolStripTextBox1.Text = i.ToString() + ";" + timer1.Interval.ToString() + ";" + flag.ToString() + "; X:" + Cursor.Position.X.ToString() +
+                "; Y:" + Cursor.Position.Y.ToString();
+            notifyIcon1.Text= i.ToString() + ";" + timer1.Interval.ToString() + ";" + flag.ToString() + "; X:" + Cursor.Position.X.ToString() +
+                "; Y:" + Cursor.Position.Y.ToString() + "\r\nВкл - правый Ctrl; Выкл - левый Ctrl";
+        }
+
+        private void Form1_Activated(object sender, EventArgs e)
+        {
+            this.Hide();
+            this.ShowInTaskbar = false;
+        }
+    }
+}
